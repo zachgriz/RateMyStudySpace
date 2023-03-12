f764073f1759862faaaa3196b3d2a3beb10ba52b
Aidan Griffin <aidan.griffin@wsu.edu>
2023-03-12 11:31:19 -0700

    added add school function

--- Controller/controllers/schoolController.js
+++ Controller/controllers/schoolController.js
@@ -23,20 +23,56 @@ exports.view = (req, res) => {
         });
 }
 
+//Search for specific school
 exports.find = (req, res) => {
-
     let searchterm = req.body.search
-
-    console.log("searchterm: ", searchterm);
 
     knex
     .select('*')
     .from("school")
-    .where('sname', 'like', '%' + searchterm +'%')
+    .where('sname', 'LIKE', '%' + searchterm +'%')
     .then((results) => {
         res.render('schoolsearch', { results: results });
 
-    console.log(results);
-
     });
+}
+
+//Add new school
+exports.schoolform = (req, res) => {
+    res.render('addschool');
+};
+
+//Add new school
+exports.schoolcreate = (req, res) => {
+    // res.render('addschool');
+
+    const {schoolname, countryname, statename, cityname} = req.body;
+    
+    console.log(schoolname, countryname, statename, cityname);
+    knex
+    .insert ({sname: schoolname,
+            country: countryname,
+            state: statename,
+            city: cityname
+    })
+    .into("school")
+    .then((results) => {
+        res.render('addschool', {alert : "School added successfully. "} );
+    });
+
+};
+
+// View selected school
+exports.view = (req, res) => {
+    // Perform database query
+    knex
+        .select()
+        .from("school")
+        .where('sname', '=', req.params.id )
+        .then((results) => {
+            res.render('viewschool', { results: results });
+
+        // console.log(results);
+
+        });
 }
--- Controller/routes/school.js
+++ Controller/routes/school.js
@@ -5,6 +5,9 @@ const schoolController = require('../controllers/schoolController');
 
 router.get('/schoolsearch', schoolController.view);
 router.post('/schoolsearch', schoolController.find);
+router.get('/addschool', schoolController.schoolform);
+router.post('/addschool', schoolController.schoolcreate);
+router.post('/viewschool/:id', schoolController.schoolview);
 
 
 
